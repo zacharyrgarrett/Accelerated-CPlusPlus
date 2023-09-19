e4e4c5bde1442d1052b217b0f740722fd57d160d
Zachary Garrett <zacharyrgarrett@gmail.com>
2023-09-18 22:51:15 -0700

    Chapters 0,1 Examples

---
+++ Ch0-GettingStarted/hello_world.cpp
@@ -0,0 +1,10 @@
+// a small C++ program
+// VS Code C++ configuration - https://code.visualstudio.com/docs/cpp/config-msvc
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
